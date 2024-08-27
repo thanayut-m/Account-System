87f60184cbf57e911b6bbf6cc26738e3c1067b89
Thanayut.m <Thanayut.mukl@gmail.com>
2024-08-27 17:12:27 +0700

    Design Pages.

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Expenses from "./pages/expenses/Expenses.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Expenses",
+    element: <Expenses />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ my-app/src/pages/expenses/Expenses.jsx
@@ -0,0 +1,40 @@
+function Expenses() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Pages Expenses</h1>
+      <div>
+        <table className="table-fixed">
+          <thead>
+            <tr>
+              <th>ลำดับ</th>
+              <th>ชื่อรายการ</th>
+              <th>รายรับ</th>
+              <th>รายจ่าย</th>
+              <th>ประเภท</th>
+              <th>เดือนที่/จำนวนเดือน</th>
+              <th>หมายเหตุ</th>
+              <th>สถานนะ</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>sdfsdfsd</td>
+              <td>fsdfsdfsdf</td>
+              <td>sdfsdfsdf</td>
+              <td>sdfsdfsdf</td>
+              <td>sdfsdfsdf</td>
+              <td>sdfsdfsdf</td>
+              <td>sdfsdfsdf</td>
+              <td>sdfsdfsdf</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Expenses;
